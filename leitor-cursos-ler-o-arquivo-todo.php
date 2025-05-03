ad202b9d85424da6e1a290ad2ac024f91e87703b
ElMarcelFarias <marcel.leitefarias@gmail.com>
2025-05-03 14:56:57 -0300

    Change: Conhecendo as funcoes file_get_contents e file

--- leitor-cursos-ler-o-arquivo-todo.php
+++ leitor-cursos-ler-o-arquivo-todo.php
@@ -6,6 +6,19 @@ $arquivo = fopen('lista-cursos.txt', 'r');
 $tamanhoDoArquivo = filesize('lista-cursos.txt'); // pega o tamanho do arquivo 
 $cursos = fread($arquivo, $tamanhoDoArquivo); //segundo parametro é para informar a quantidade de bits que quer ler 
 
-echo $cursos;
+// echo $cursos;
 
 fclose($arquivo);
+
+
+// SIMPLIFICANDO
+
+$cursosSimplificado = file_get_contents('lista-cursos.txt');
+// abrir o arquivo, buscar o conteudo e fechar o arquivo. Tudo em uma única funcao 
+// n sao todos os casos que vamos precisar disso
+
+
+$arrayCursosSimplificado = file('lista-cursos.txt'); //Retorna as linhas do texto para um index do array 
+
+
+echo $cursosSimplificado;
